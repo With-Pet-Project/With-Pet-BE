a26ca163b5befba4a9cb97ce203281a84ff466ee
Jin409 <sualng123@gmail.com>
2023-01-29 17:58:58 +0900

    feat: ApiResponse 생성자 추가

--- src/main/java/WebProject/withpet/common/dto/ApiResponse.java
+++ src/main/java/WebProject/withpet/common/dto/ApiResponse.java
@@ -1,14 +1,21 @@
 package WebProject.withpet.common.dto;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Builder
-@Setter
 public class ApiResponse<T> {
     private final int code;
     private final String message;
-    private final T data;
+    private T data;
+
+    public ApiResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public ApiResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
