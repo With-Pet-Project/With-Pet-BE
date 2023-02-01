e1838e10640f0fe6a522933ce089807309bc6d9c
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-01 18:09:24 +0900

    feat: 에러 응답 형식 지정한 클래스

---
+++ src/main/java/WebProject/withpet/common/dto/ApiErrorResponse.java
@@ -0,0 +1,27 @@
+package WebProject.withpet.common.dto;
+
+import WebProject.withpet.common.constants.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Data
+@Builder
+public class ApiErrorResponse {
+
+    private int code;
+    private String message;
+
+    public static ResponseEntity<ApiErrorResponse> toResponseEntity(ErrorCode e) {
+        return ResponseEntity
+            .status(e.getHttpStatus())
+            .body(
+                ApiErrorResponse.builder()
+                    .code(e.getHttpStatus().value())
+                    .message(e.getMessage())
+                    .build());
+    }
+}
