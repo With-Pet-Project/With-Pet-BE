6e435b6b0d51ec0cf9263697f5e5b89eb759c8b9
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-11 14:39:27 +0900

    feat: kakao 소셜 로그인 controller 추가

--- src/main/java/WebProject/withpet/users/controller/UserController.java
+++ src/main/java/WebProject/withpet/users/controller/UserController.java
@@ -3,13 +3,18 @@ package WebProject.withpet.users.controller;
 import WebProject.withpet.common.auth.application.JwtTokenProvider;
 import WebProject.withpet.common.constants.ResponseConstants;
 import WebProject.withpet.common.dto.ApiResponse;
+import WebProject.withpet.users.dto.SocialLoginResponseDto;
 import WebProject.withpet.users.dto.UserRequestDto;
 import WebProject.withpet.users.service.UserService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,4 +31,15 @@ public class UserController {
         userService.register(user);
         return ResponseConstants.RESPONSE_SAVE_OK;
     }
+
+    @PostMapping("kakao/login")
+    public ResponseEntity<ApiResponse<SocialLoginResponseDto>> socialLogin(@RequestParam(name="code") String code) throws JSONException{
+
+        ApiResponse<SocialLoginResponseDto> socialLongResponse = new ApiResponse<>(200, "카카오 로그인 성공",
+            userService.socialLogin(code));
+
+        return ResponseEntity.status(HttpStatus.OK).body(socialLongResponse);
+
+    }
+
 }
