4cbf9a9b4b16efebbe00336734c75ee21373cb62
Jin409 <sualng123@gmail.com>
2023-02-11 16:24:46 +0900

    fix: 인가 과정 버그 수정

--- src/main/java/WebProject/withpet/common/config/WebConfig.java
+++ src/main/java/WebProject/withpet/common/config/WebConfig.java
@@ -1,20 +1,20 @@
 package WebProject.withpet.common.config;
 
+import WebProject.withpet.common.auth.application.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private final String[] INTERCEPTOR_WHITE_LIST = {
-            "/user",
-            "/user/signup"
-    };
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new JwtAuthInterceptor())
-                .addPathPatterns("/*")
-                .excludePathPatterns("/user", "/user/signup");
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtAuthInterceptor(jwtTokenProvider))
+                .addPathPatterns("/pet/**");
     }
 }
