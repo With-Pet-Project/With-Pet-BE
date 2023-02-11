ccdcd096838296d58eafa164c5e5aef9f6604e42
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-11 23:45:23 +0900

    feat: web 설정에 /mypage/** 추가

--- src/main/java/WebProject/withpet/common/config/WebConfig.java
+++ src/main/java/WebProject/withpet/common/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtAuthInterceptor(jwtTokenProvider))
-                .addPathPatterns("/pet/**");
+                .addPathPatterns("/pet/**","/mypage/**");
     }
 }
