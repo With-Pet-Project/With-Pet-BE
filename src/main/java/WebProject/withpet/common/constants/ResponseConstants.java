6c32652d38e76a47fe5dcd1727fbb39467fa26cf
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-14 14:04:30 +0900

    feat: ApiResponse<Void> DUPLICATE_CHECK_OK 추가

--- src/main/java/WebProject/withpet/common/constants/ResponseConstants.java
+++ src/main/java/WebProject/withpet/common/constants/ResponseConstants.java
@@ -3,8 +3,16 @@ package WebProject.withpet.common.constants;
 import WebProject.withpet.common.dto.ApiResponse;
 
 public class ResponseConstants {
-    public static final ApiResponse<Void> RESPONSE_SAVE_OK = new ApiResponse<>(201, "정상적으로 저장되었습니다.");
-    public static final ApiResponse<Void> RESPONSE_VIEW_OK = new ApiResponse<>(200, "정상적으로 조회되었습니다.");
-    public static final ApiResponse<Void> RESPONSE_DEL_OK = new ApiResponse<>(200, "정상적으로 삭제되었습니다.");
-    public static final ApiResponse<Void> RESPONSE_UPDATE_OK = new ApiResponse<>(200, "정상적으로 수정되었습니다.");
+
+    public static final ApiResponse<Void> RESPONSE_SAVE_OK = new ApiResponse<>(201,
+        "정상적으로 저장되었습니다.");
+    public static final ApiResponse<Void> RESPONSE_VIEW_OK = new ApiResponse<>(200,
+        "정상적으로 조회되었습니다.");
+    public static final ApiResponse<Void> RESPONSE_DEL_OK = new ApiResponse<>(200,
+        "정상적으로 삭제되었습니다.");
+    public static final ApiResponse<Void> RESPONSE_UPDATE_OK = new ApiResponse<>(200,
+        "정상적으로 수정되었습니다.");
+
+    public static final ApiResponse<Void> DUPLICATE_CHECK_OK = new ApiResponse<>(200,
+        "사용 가능한 닉네임입니다");
 }
