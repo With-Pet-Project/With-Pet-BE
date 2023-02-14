13f4259140478f8ce004855ffb62a8b8f4eda50d
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-14 14:05:39 +0900

    feat: 닉네임 중복 확인하는 controller 추가

--- src/main/java/WebProject/withpet/users/controller/UserController.java
+++ src/main/java/WebProject/withpet/users/controller/UserController.java
@@ -6,14 +6,23 @@ import WebProject.withpet.common.constants.ResponseConstants;
 import WebProject.withpet.common.dto.ApiResponse;
 import WebProject.withpet.users.dto.SocialLoginResponseDto;
 import WebProject.withpet.users.dto.UserChangeInfoRequestDto;
+import WebProject.withpet.users.dto.UserNickNameDuplicateCheckDto;
 import WebProject.withpet.users.dto.UserRequestDto;
 import WebProject.withpet.users.service.UserService;
+import java.util.HashMap;
+import java.util.Map;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,6 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -38,25 +48,24 @@ public class UserController {
     }
 
     @PostMapping("kakao/login")
-    public ResponseEntity<ApiResponse<SocialLoginResponseDto>> socialLogin(@RequestParam(name = "code") String code)
-            throws JSONException {
+    public ResponseEntity<ApiResponse<SocialLoginResponseDto>> socialLogin(
+        @RequestParam(name = "code") String code)
+        throws JSONException {
 
-        ApiResponse<SocialLoginResponseDto> socialLongResponse = new ApiResponse<>(200, "카카오 로그인 성공",
-                userService.socialLogin(code));
+        ApiResponse<SocialLoginResponseDto> socialLongResponse = new ApiResponse<>(200,
+            "카카오 로그인 성공",
+            userService.socialLogin(code));
 
         return ResponseEntity.status(HttpStatus.OK).body(socialLongResponse);
 
     }
 
-    @PutMapping
-    public ResponseEntity<ApiResponse<Void>> changeUserInfo(
-        @AuthenticationPrincipal PrincipalDetails principalDetails,
-        @Valid @RequestBody UserChangeInfoRequestDto userChangeInfoRequestDto){
-
-        userService.changeUserInfo(principalDetails.getUser(),userChangeInfoRequestDto);
-
-
-        return ResponseEntity.status(HttpStatus.OK).body(ResponseConstants.RESPONSE_UPDATE_OK);
+    @GetMapping("/duplicate-check")
+    public ResponseEntity<ApiResponse<Void>> duplicateCheck(
+        @RequestParam("nickName") @NotBlank(message = "닉네임을 입력하시오") String nickName
+    ) {
+        userService.validateDuplicateNickname(nickName);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseConstants.DUPLICATE_CHECK_OK);
     }
 
 }
