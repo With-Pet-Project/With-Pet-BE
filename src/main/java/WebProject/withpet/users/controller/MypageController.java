652ff956d496c83ace620e1785620f1f92876718
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-19 17:51:48 +0900

    feat: MypageController에 마이페이지 조회 구현

--- src/main/java/WebProject/withpet/users/controller/MypageController.java
+++ src/main/java/WebProject/withpet/users/controller/MypageController.java
@@ -4,12 +4,14 @@ import WebProject.withpet.common.auth.PrincipalDetails;
 import WebProject.withpet.common.constants.ResponseConstants;
 import WebProject.withpet.common.dto.ApiResponse;
 import WebProject.withpet.users.dto.MypageChangeRequestDto;
+import WebProject.withpet.users.dto.ViewMypageResponseDto;
 import WebProject.withpet.users.service.MypageService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +25,7 @@ public class MypageController {
 
     private final MypageService mypageService;
 
-    @PatchMapping ()
+    @PatchMapping()
     public ResponseEntity<ApiResponse<Void>> changeUserInfo(
         @AuthenticationPrincipal PrincipalDetails principalDetails,
         @Valid @RequestBody MypageChangeRequestDto mypageChangeRequestDto) {
@@ -31,5 +33,15 @@ public class MypageController {
         mypageService.changeUserInfo(principalDetails.getUser(), mypageChangeRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseConstants.RESPONSE_UPDATE_OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<ViewMypageResponseDto>> viewMypage(
+        @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        ApiResponse<ViewMypageResponseDto> response = new ApiResponse<>(200,
+            "마이페이지 성공적으로 조회 성공", mypageService.viewMypage(principalDetails.getUser()));
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(response);
     }
 }
