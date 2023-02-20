30d6e31c13e8c5c26d500020f60c5d5528ade51c
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-20 18:48:03 +0900

    refactor: 커스텀 리포지토리를 위한 ArticleRepository 리팩토링

--- src/main/java/WebProject/withpet/articles/repository/ArticleRepository.java
+++ src/main/java/WebProject/withpet/articles/repository/ArticleRepository.java
@@ -1,16 +1,16 @@
 package WebProject.withpet.articles.repository;
 
 import WebProject.withpet.articles.domain.Article;
+import WebProject.withpet.articles.domain.SpecArticle;
 import WebProject.withpet.articles.dto.MypageArticleDto;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+@Repository
+public interface ArticleRepository< T extends Article> extends JpaRepository<T, Long>, ArticleRepositoryCustom {
 
-    @Query(
-        "select new WebProject.withpet.articles.dto.MypageArticleDto(a.id, a.createdTime, a.detailText) "
-            + "from Article a where a.user.id = :userId")
-    List<MypageArticleDto> findMypageArticelsByUserId(@Param("userId") Long userId);
 }
