fbb10adeeb4d91ec77d775b795081d6ec5729ce0
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-20 18:54:50 +0900

    feat: article 관련 시큐리티 추가

--- src/main/java/WebProject/withpet/common/config/WebConfig.java
+++ src/main/java/WebProject/withpet/common/config/WebConfig.java
@@ -16,6 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtAuthInterceptor(jwtTokenProvider))
 
-                .addPathPatterns("/pet/**", "/mypage/**", "/{petId}/challenge/**");
+                .addPathPatterns("/pet/**", "/mypage/**", "/{petId}/challenge/**","/article/**");
     }
 }
