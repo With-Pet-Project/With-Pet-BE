1567381b2d660104a7996fabfaecd4ca9eb42e62
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-22 01:11:05 +0900

    feat: S3에 이미지 업로드 로직과 게시글 상세 조회 로직 추가 및 이미지 엔티티 생성 로직 리팩토링

--- src/main/java/WebProject/withpet/articles/service/ArticleService.java
+++ src/main/java/WebProject/withpet/articles/service/ArticleService.java
@@ -6,14 +6,19 @@ import WebProject.withpet.articles.domain.Image;
 import WebProject.withpet.articles.domain.SpecArticle;
 import WebProject.withpet.articles.domain.Tag;
 import WebProject.withpet.articles.dto.ArticleCreateRequestDto;
-import WebProject.withpet.articles.dto.ArticleCreateRequestImgDto;
+import WebProject.withpet.articles.dto.FileDto;
+import WebProject.withpet.articles.dto.ViewSpecificArticleResponseDto;
+import WebProject.withpet.articles.dto.ViewUserAndArticleResponseDto;
 import WebProject.withpet.articles.repository.ArticleRepository;
 import WebProject.withpet.articles.repository.ImageRepository;
+import WebProject.withpet.common.file.AwsS3Service;
 import WebProject.withpet.users.domain.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -24,53 +29,64 @@ public class ArticleService {
 
     private final ImageRepository imageRepository;
 
+    private final AwsS3Service awsS3Service;
+
     @Transactional
-    public void createArticle(User user, ArticleCreateRequestDto articleCreateRequestDto) {
+    public void createArticle(User user, ArticleCreateRequestDto articleCreateRequestDto,
+        List<MultipartFile> files) {
 
         if (articleCreateRequestDto.getTag().equals(Tag.LOST) || articleCreateRequestDto.getTag()
             .equals(Tag.HOSPITAL) || articleCreateRequestDto.getTag().equals(Tag.WALK)) {
-            SpecArticle specArticle = articleCreateRequestDto.toSpecArticleEntity();
 
+            SpecArticle specArticle = articleCreateRequestDto.toSpecArticleEntity(user);
             articleRepository.save(specArticle);
-            articleRepository.flush();
-            log.info("size:::::::{}",articleCreateRequestDto.getImages().size());
-            if (articleCreateRequestDto.getImages().size() != 0) {
-                createImageEntityAndCreateRelation(articleCreateRequestDto, specArticle);
-            }
+
+            createImgAndInjectAwsImgUrl(specArticle, files);
+
 
         } else {
 
-            Article article = articleCreateRequestDto.toArticleEntity();
+            Article article = articleCreateRequestDto.toArticleEntity(user);
             articleRepository.save(article);
-            articleRepository.flush();
-            log.info("size:::::::{}",articleCreateRequestDto.getImages().size());
-            if (articleCreateRequestDto.getImages().size() != 0) {
-                createImageEntityAndCreateRelation(articleCreateRequestDto, article);
-            }
+
+            createImgAndInjectAwsImgUrl(article, files);
         }
+    }
+
+
+    @Transactional
+    public ViewSpecificArticleResponseDto viewSpecificArticle(Long articleId) {
+
+        ViewUserAndArticleResponseDto result = articleRepository.findSpecificArticle(
+            articleId);
+
+        ViewSpecificArticleResponseDto response = ViewSpecificArticleResponseDto.builder()
+            .profileImg(result.getProfileImg())
+            .nickName(result.getNickName())
+            .createdTime(result.getArticle().getCreatedTime())
+            .modifiedTime(result.getArticle().getModifiedTime())
+            .detailText(result.getArticle().getDetailText())
+            .likeCnt(result.getArticle().getLikeCnt())
+            .build();
+
+        for (Image image : result.getArticle().getImages()) {
+            response.getImages().add(image.getContent());
+        }
+
+        return response;
     }
 
     @Transactional
-    public void createImageEntityAndCreateRelation(ArticleCreateRequestDto articleCreateRequestDto,
-        Article article) {
-        log.info("여기까지 진행1");
-        log.info("{}",articleCreateRequestDto.getImages().get(0).getContent());
-        log.info("{}",articleCreateRequestDto.getImages().get(1).getContent());
-        for (ArticleCreateRequestImgDto dto : articleCreateRequestDto.getImages()) {
+    public void createImgAndInjectAwsImgUrl(Article article, List<MultipartFile> multipartFiles) {
+        List<String> AwsImgUrl = awsS3Service.uploadImage(multipartFiles);
 
-            log.info("여기까지 진행2");
-            log.info("articl: {}", article.getCreatedTime());
-            log.info("content: {}",dto.getContent());
-            Image img = Image.builder()
+        AwsImgUrl.forEach(url -> {
+
+            imageRepository.save(Image.builder()
                 .article(article)
-                .content(dto.getContent())
-                .build();
-            img.setArticle(article);
-            imageRepository.save(img);
-
-        }
+                .content(url)
+                .build());
+        });
     }
-
-
 }
 
