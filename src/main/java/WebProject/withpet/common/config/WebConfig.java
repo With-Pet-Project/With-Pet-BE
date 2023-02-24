3c92d5c3d8afb3b8268001d0c2f4fc5a84e552ba
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-25 00:45:56 +0900

    feat: CORS 문제 해결을 위한 헤더 설정 추가

--- src/main/java/WebProject/withpet/common/config/WebConfig.java
+++ src/main/java/WebProject/withpet/common/config/WebConfig.java
@@ -25,6 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
+            .allowedHeaders("*")
             .allowedMethods("*");
 
     }
