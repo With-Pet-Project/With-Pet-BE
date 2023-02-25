880be7ded8d1fae64b17cb74daad3c78892e512e
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-25 14:43:14 +0900

    feat: CORS 문제 해결을 위한 시큐리티 설정 추가

--- src/main/java/WebProject/withpet/common/config/WebConfig.java
+++ src/main/java/WebProject/withpet/common/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("*","http://localhost:3000")
             .allowedHeaders("*")
             .allowedMethods("*");
 
