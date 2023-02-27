6b3799a27360ad8a3376a29dc472dc840b6a7df5
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-02-27 13:11:13 +0900

    feat: 게시글 상세 조회 시 댓글도 보이도록 코드 추가 ,이미지 url json 반환 형태 변경

--- src/main/java/WebProject/withpet/articles/dto/ViewSpecificArticleResponseDto.java
+++ src/main/java/WebProject/withpet/articles/dto/ViewSpecificArticleResponseDto.java
@@ -1,5 +1,7 @@
 package WebProject.withpet.articles.dto;
 
+import WebProject.withpet.comments.domain.Comment;
+import WebProject.withpet.comments.dto.ViewCommentList;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,18 +27,29 @@ public class ViewSpecificArticleResponseDto {
 
     private Integer likeCnt;
 
-    private List<String> images= new ArrayList<>();
+    private Integer commentCnt;
+
+
+    private List<ImageDto> images= new ArrayList<>();
+
+    private List<ViewCommentList> commentList = new ArrayList<>();
 
     @Builder
     public ViewSpecificArticleResponseDto(String profileImg, String nickName,
         LocalDateTime createdTime,
-        LocalDateTime modifiedTime, String detailText, Integer likeCnt) {
+        LocalDateTime modifiedTime, String detailText, Integer likeCnt,Integer commentCnt) {
         this.profileImg = profileImg;
         this.nickName = nickName;
         this.createdTime = createdTime;
         this.modifiedTime = modifiedTime;
         this.detailText = detailText;
         this.likeCnt = likeCnt;
-
+        this.commentCnt=commentCnt;
     }
+
+    public void setCommentListAndCommentCnt(List<ViewCommentList> commentList, Integer commentCnt) {
+        this.commentList = commentList;
+        this.commentCnt=commentCnt;
+    }
+
 }
