29e9d7ab71a2e1e0499cf94c1b9d29770bb27025
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-02 21:25:51 +0900

    feat: 승희님 피드백 반영하여 Dto 네이밍 수정

--- src/main/java/WebProject/withpet/comments/repository/CommentRepositoryCustom.java
+++ src/main/java/WebProject/withpet/comments/repository/CommentRepositoryCustom.java
@@ -1,7 +1,7 @@
 package WebProject.withpet.comments.repository;
 
 import WebProject.withpet.comments.domain.Comment;
-import WebProject.withpet.comments.dto.ViewCommentList;
+import WebProject.withpet.comments.dto.ViewCommentListDto;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepositoryCustom {
 
-    Slice<ViewCommentList> getCommentsList(Long lastCommentId, Long articleId, Pageable pageable);
+    Slice<ViewCommentListDto> getCommentsList(Long lastCommentId, Long articleId, Pageable pageable);
 
     List<Comment> getChildrenListById(Long parentId);
 }
