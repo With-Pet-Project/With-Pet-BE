35632821f78e37cfeb4e13f8b2f0edfbaa8fe524
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-02 21:26:20 +0900

    refactor: 승희님 피드백 반영하여 Dto 네이밍 수정

--- src/main/java/WebProject/withpet/comments/repository/CommentRepositoryImpl.java
+++ src/main/java/WebProject/withpet/comments/repository/CommentRepositoryImpl.java
@@ -6,14 +6,12 @@ import static WebProject.withpet.users.domain.QUser.user;
 
 import WebProject.withpet.comments.domain.Comment;
 import WebProject.withpet.comments.domain.QComment;
-import WebProject.withpet.comments.dto.ViewCommentList;
-import WebProject.withpet.users.domain.QUser;
+import WebProject.withpet.comments.dto.ViewCommentListDto;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -26,13 +24,13 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
     private final EntityManager em;
 
     @Override
-    public Slice<ViewCommentList> getCommentsList(Long lastCommentId, Long articleId,
+    public Slice<ViewCommentListDto> getCommentsList(Long lastCommentId, Long articleId,
         Pageable pageable) {
 
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
 
-        List<ViewCommentList> commentList = queryFactory
-            .select(Projections.constructor(ViewCommentList.class,
+        List<ViewCommentListDto> commentList = queryFactory
+            .select(Projections.constructor(ViewCommentListDto.class,
                 comment.id, user.profileImg, user.nickName, comment.createdTime,
                 comment.modifiedTime, comment.content))
             .from(comment)
@@ -64,7 +62,7 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
 
     }
 
-    private Slice<ViewCommentList> checkLastPage(List<ViewCommentList> commentList,
+    private Slice<ViewCommentListDto> checkLastPage(List<ViewCommentListDto> commentList,
         Pageable pageable) {
 
         Boolean hasNext = false;
