75235c50e6ee35d2c77aa9b2f2da2d9ffeec21fc
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-02 21:03:08 +0900

    feat: 이미지 S3에 저장 후 Url 반환하는 이미지 등록, 이미지 삭제 controller 구현

---
+++ src/main/java/WebProject/withpet/articles/controller/ImageController.java
@@ -0,0 +1,48 @@
+package WebProject.withpet.articles.controller;
+
+
+import WebProject.withpet.articles.domain.Image;
+import WebProject.withpet.articles.dto.ImageDto;
+import WebProject.withpet.articles.service.ImageService;
+import WebProject.withpet.common.constants.ResponseConstants;
+import WebProject.withpet.common.dto.ApiResponse;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/image")
+    public ResponseEntity<ApiResponse<List<ImageDto>>> registerImage(
+        @RequestPart List<MultipartFile> images) {
+
+        ApiResponse<List<ImageDto>> response = new ApiResponse<>(200,
+            "이미지가 성공적으로 등록되었습니다", imageService.registerImages(images));
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+
+    @DeleteMapping("/image")
+    public ResponseEntity<ApiResponse<Void>> deleteImage(@RequestBody ImageDto imageDto) {
+
+        imageService.deleteImage(imageDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseConstants.RESPONSE_DEL_OK);
+
+    }
+}
