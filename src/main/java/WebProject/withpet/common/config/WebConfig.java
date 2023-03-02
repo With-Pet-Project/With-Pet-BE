030a79cb38a78328a7edf64f5570781d5a8ba5c1
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-02 21:38:26 +0900

    feat: api 추가에 따른 설정 추가

--- src/main/java/WebProject/withpet/common/config/WebConfig.java
+++ src/main/java/WebProject/withpet/common/config/WebConfig.java
@@ -17,14 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtAuthInterceptor(jwtTokenProvider))
 
-            .addPathPatterns("/pet/**", "/mypage/**", "/{petId}/challenge/**", "/article/**");
+            .addPathPatterns("/pet/**", "/mypage/**", "/{petId}/challenge/**", "/article/**",
+                "/image/**", "/comment/**", "/comments/**");
 
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*","http://localhost:3000")
+            .allowedOrigins("*", "http://localhost:3000")
             .allowedHeaders("*")
             .allowedMethods("*");
 
