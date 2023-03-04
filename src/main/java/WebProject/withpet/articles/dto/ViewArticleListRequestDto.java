5228e9042c71013ea1100dd9c77b121a1c919d38
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-04 22:43:37 +0900

    feat: 게시글 무한 스크롤 요청 시 생성하는 dto 추가

---
+++ src/main/java/WebProject/withpet/articles/dto/ViewArticleListRequestDto.java
@@ -0,0 +1,28 @@
+package WebProject.withpet.articles.dto;
+
+import WebProject.withpet.articles.domain.Filter;
+import WebProject.withpet.articles.domain.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ViewArticleListRequestDto {
+
+    private Tag tag;
+
+    private Filter filter;
+
+    private String place1;
+
+    private String place2;
+
+    private Long lastArticleId;
+
+    private Integer size;
+
+}
