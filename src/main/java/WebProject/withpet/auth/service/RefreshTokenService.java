0c0d57092451db534f074e4aea883b0a8b08cd90
Jin409 <sualng123@gmail.com>
2023-03-05 22:48:47 +0900

    feat: refresh token 엔티티 구성 및 생성 혹은 교환 기능 구현

---
+++ src/main/java/WebProject/withpet/auth/service/RefreshTokenService.java
@@ -0,0 +1,21 @@
+package WebProject.withpet.auth.service;
+
+import WebProject.withpet.auth.domain.RefreshToken;
+import WebProject.withpet.auth.domain.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public void createOrChangeRefreshToken(String refreshToken, Long userId) {
+        refreshTokenRepository.findByUserId(userId).ifPresentOrElse(
+                (tokenEntity) -> tokenEntity.changeToken(refreshToken),
+                () -> refreshTokenRepository.save(new RefreshToken(refreshToken, userId))
+        );
+    }
+}
