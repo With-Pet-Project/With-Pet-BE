21b87f1c42e8dcbf11a0ec40457b05357f86965d
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-05 18:26:22 +0900

    feat: 게시글 페이지네이션 인터페이스 인자로 마지막 조회한 게시글 추가

--- src/main/java/WebProject/withpet/articles/repository/ArticleRepositoryPagingCustom.java
+++ src/main/java/WebProject/withpet/articles/repository/ArticleRepositoryPagingCustom.java
@@ -1,5 +1,6 @@
 package WebProject.withpet.articles.repository;
 
+import WebProject.withpet.articles.domain.Article;
 import WebProject.withpet.articles.dto.ViewArticleListDto;
 import WebProject.withpet.articles.dto.ViewArticleListRequestDto;
 import org.springframework.data.domain.Pageable;
@@ -10,5 +11,6 @@ public interface ArticleRepositoryPagingCustom {
     //주석 처리된 인터페이스는 추후 성능 최적화할때 테스트용 사용하고자 만들었습니다.
     //Slice<ViewArticleListDto> getArticlesList2(ViewArticleListRequestDto dto, Pageable pageable);
 
-    Slice<ViewArticleListDto> getArticleList(ViewArticleListRequestDto dto, Pageable pageable);
+    Slice<ViewArticleListDto> getArticleList(Article lastArticle, ViewArticleListRequestDto dto,
+        Pageable pageable);
 }
