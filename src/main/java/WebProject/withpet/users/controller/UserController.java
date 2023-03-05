73b88a28705a3b475d4a070a0768a8187529aa1f
Jin409 <sualng123@gmail.com>
2023-03-05 22:49:26 +0900

    feat: 로그인 시 access token 을 payload 에, refresh token 을 쿠키에 담아 반환하는 기능 구현

--- src/main/java/WebProject/withpet/users/controller/UserController.java
+++ src/main/java/WebProject/withpet/users/controller/UserController.java
@@ -1,18 +1,21 @@
 package WebProject.withpet.users.controller;
 
-import WebProject.withpet.common.auth.PrincipalDetails;
-import WebProject.withpet.common.auth.application.JwtTokenProvider;
+import WebProject.withpet.auth.PrincipalDetails;
+import WebProject.withpet.auth.dto.TokenResponseDto;
+import WebProject.withpet.auth.vo.LoginVo;
 import WebProject.withpet.common.constants.ResponseConstants;
 import WebProject.withpet.common.dto.ApiResponse;
 import WebProject.withpet.users.dto.ChangePasswordDto;
 import WebProject.withpet.users.dto.SocialLoginResponseDto;
 import WebProject.withpet.users.dto.UserRequestDto;
 import WebProject.withpet.users.service.UserService;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -31,9 +34,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class UserController {
 
-    private final JwtTokenProvider jwtTokenProvider;
+    @Value("${jwt.cookie-valid-time}")
+    private Long cookieValidTime;
     private final UserService userService;
-
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signUp(@Valid @RequestBody UserRequestDto user) {
@@ -41,14 +44,27 @@ public class UserController {
         return ResponseEntity.ok(ResponseConstants.RESPONSE_SAVE_OK);
     }
 
-    @PostMapping("/kakao/login")
-    public ResponseEntity<ApiResponse<SocialLoginResponseDto>> socialLogin(
-        @RequestParam(name = "code") String code)
-        throws JSONException {
+    @PostMapping
+    public ResponseEntity<ApiResponse<String>> signIn(@Valid @RequestBody LoginVo request,
+                                                      HttpServletResponse response) {
+        TokenResponseDto tokenResponseDto = userService.login(request.getEmail(), request.getPassword());
 
-        ApiResponse<SocialLoginResponseDto> socialLongResponse = new ApiResponse<>(200,
-            "카카오 로그인 성공",
-            userService.socialLogin(code));
+        Cookie cookie = new Cookie("refresh-token", tokenResponseDto.getRefreshToken());
+        cookie.setMaxAge(Math.toIntExact(cookieValidTime));
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+
+        ApiResponse<String> apiResponse = new ApiResponse<>(200, "로그인 되었습니다.", tokenResponseDto.getAccessToken());
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PostMapping("/kakao/login")
+
+    public ResponseEntity<ApiResponse<SocialLoginResponseDto>> socialLogin(@RequestParam(name = "code") String code)
+            throws JSONException {
+
+        ApiResponse<SocialLoginResponseDto> socialLongResponse = new ApiResponse<>(200, "카카오 로그인 성공",
+                userService.socialLogin(code));
 
         return ResponseEntity.status(HttpStatus.OK).body(socialLongResponse);
 
@@ -56,16 +72,14 @@ public class UserController {
 
     @GetMapping("/duplicate-check")
     public ResponseEntity<ApiResponse<Void>> duplicateCheck(
-        @RequestParam("nickName") @NotBlank(message = "닉네임을 입력하시오") String nickName
-    ) {
+            @RequestParam("nickName") @NotBlank(message = "닉네임을 입력하시오") String nickName) {
         userService.validateDuplicateNickname(nickName);
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200, "사용 가능한 닉네임입니다"));
     }
 
     @PostMapping("/password")
-    public ResponseEntity<ApiResponse<Void>> changePassword(
-        @AuthenticationPrincipal PrincipalDetails principalDetails,
-        @RequestBody @Valid ChangePasswordDto changePasswordDto) {
+    public ResponseEntity<ApiResponse<Void>> changePassword(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                            @RequestBody @Valid ChangePasswordDto changePasswordDto) {
 
         userService.changePassword(principalDetails.getUser(), changePasswordDto);
 
@@ -73,11 +87,12 @@ public class UserController {
     }
 
     @DeleteMapping()
-    public ResponseEntity<ApiResponse<Void>> deleteUser(
-        @AuthenticationPrincipal PrincipalDetails principalDetails) {
+    public ResponseEntity<ApiResponse<Void>> deleteUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
         userService.deleteUser(principalDetails.getUser());
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseConstants.RESPONSE_DEL_OK);
     }
 }
+
+
--- src/main/java/WebProject/withpet/users/service/UserService.java
+++ src/main/java/WebProject/withpet/users/service/UserService.java
@@ -1,8 +1,12 @@
 package WebProject.withpet.users.service;
 
-import WebProject.withpet.common.auth.application.JwtTokenProvider;
+import WebProject.withpet.auth.PrincipalDetails;
+import WebProject.withpet.auth.application.JwtTokenProvider;
+import WebProject.withpet.auth.dto.TokenResponseDto;
+import WebProject.withpet.auth.service.RefreshTokenService;
 import WebProject.withpet.common.constants.ErrorCode;
 import WebProject.withpet.common.exception.DuplicateException;
+import WebProject.withpet.common.exception.UserNotFoundException;
 import WebProject.withpet.users.domain.User;
 import WebProject.withpet.users.dto.ChangePasswordDto;
 import WebProject.withpet.users.dto.SocialLoginResponseDto;
@@ -12,14 +16,13 @@ import WebProject.withpet.users.repository.UserRepository;
 import java.util.Optional;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.json.JSONException;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -31,16 +34,14 @@ public class UserService {
     private final UserSocialService userSocialService;
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenService refreshTokenService;
 
     public void register(@Valid UserRequestDto userRequestDto) {
         validateDuplicateEmail(userRequestDto.getEmail());
         validateDuplicateNickname(userRequestDto.getNickname());
 
-        User user = User.builder()
-            .nickName(userRequestDto.getNickname())
-            .email(userRequestDto.getEmail())
-            .password(passwordEncoder.encode(userRequestDto.getPassword()))
-            .build();
+        User user = User.builder().nickName(userRequestDto.getNickname()).email(userRequestDto.getEmail())
+                .password(passwordEncoder.encode(userRequestDto.getPassword())).build();
 
         userRepository.save(user);
     }
@@ -56,23 +57,15 @@ public class UserService {
         if (userRepository.findByEmail(userInfoByToken.getEmail()).isEmpty()) {
 
             //회원가입 진행 후 토큰 생성
-            userRepository.save(
-                User.builder()
-                    .email(userInfoByToken.getEmail())
-                    .password("")
-                    .nickName(userInfoByToken.getNickname())
-                    .build()
-            );
+            userRepository.save(User.builder().email(userInfoByToken.getEmail()).password("")
+                    .nickName(userInfoByToken.getNickname()).build());
 
             createdToken = jwtTokenProvider.createToken(userInfoByToken.toEntity());
 
         } else {
             createdToken = jwtTokenProvider.createToken(userInfoByToken.toEntity());
         }
-        return SocialLoginResponseDto
-            .builder()
-            .token(createdToken)
-            .build();
+        return SocialLoginResponseDto.builder().token(createdToken).build();
     }
 
 
@@ -101,8 +94,34 @@ public class UserService {
     }
 
     @Transactional
-    public void deleteUser(User user){
+    public void deleteUser(User user) {
 
         userRepository.deleteById(user.getId());
+    }
+
+    @Transactional
+    public TokenResponseDto login(String email, String password) {
+        PrincipalDetails principalDetails = loadUserByEmail(email);
+
+        if (!passwordEncoder.matches(password, principalDetails.getPassword())) {
+            throw new UserNotFoundException();
+        }
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails.getUsername(),
+                principalDetails.getPassword(), principalDetails.getAuthorities());
+
+        // refresh Token 생성
+        String refreshToken = jwtTokenProvider.createRefreshToken(principalDetails.getUser());
+        refreshTokenService.createOrChangeRefreshToken(refreshToken, principalDetails.getUser().getId());
+
+        String accessToken = jwtTokenProvider.createToken(principalDetails.getUser());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new TokenResponseDto(refreshToken, accessToken);
+    }
+
+    private PrincipalDetails loadUserByEmail(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        return new PrincipalDetails(user);
     }
 }
