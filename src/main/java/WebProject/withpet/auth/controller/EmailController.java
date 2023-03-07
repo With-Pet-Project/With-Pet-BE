7a12fa681be0b7d0e48f4f6752beb0da82d3fdae
Jin409 <sualng123@gmail.com>
2023-03-07 15:21:25 +0900

    feat: 이메일 전송 엔드포인트 간단하게 수정 및 인증 코드 확인 기능 컨트롤러에 추가

--- src/main/java/WebProject/withpet/auth/controller/EmailController.java
+++ src/main/java/WebProject/withpet/auth/controller/EmailController.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +22,18 @@ public class EmailController {
     private final EmailService emailService;
     private final UserService userService;
 
-    @PostMapping("/request-confirm-email")
+    @PostMapping("/request-confirm")
     public ResponseEntity<ApiResponse<Void>> requestConfirmEmail(@RequestParam @NotBlank @Email String requestEmail)
             throws MessagingException, UnsupportedEncodingException {
         String confirmationKey = userService.generateConfirmationToken(requestEmail, LocalDateTime.now());
         emailService.sendEmail(requestEmail, confirmationKey);
         return ResponseEntity.ok(new ApiResponse<>(200, "정상적으로 이메일이 전송되었습니다.", null));
+    }
+
+    @GetMapping("/check-confirm")
+    public ResponseEntity<ApiResponse<Void>> checkConfirmEmail(@RequestParam @NotBlank @Email String requestEmail,
+                                                               @RequestParam @NotBlank String key) {
+        userService.permissionCheckByConfirmationToken(requestEmail, key, LocalDateTime.now());
+        return ResponseEntity.ok(new ApiResponse<>(200, "인증된 사용자입니다.", null));
     }
 }
