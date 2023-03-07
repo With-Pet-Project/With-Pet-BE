366d1030e45421bebacff3633b4d9f9d76e5056c
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-07 20:54:22 +0900

    feat: 마이페이지 수정 api controller에서 데이터 입력 받는 형식 변경

--- src/main/java/WebProject/withpet/users/controller/MypageController.java
+++ src/main/java/WebProject/withpet/users/controller/MypageController.java
@@ -7,6 +7,7 @@ import WebProject.withpet.common.dto.ApiResponse;
 import WebProject.withpet.users.dto.MypageChangeRequestDto;
 import WebProject.withpet.users.dto.ViewMypageResponseDto;
 import WebProject.withpet.users.service.MypageService;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,20 +29,23 @@ public class MypageController {
     private final MypageService mypageService;
 
     @PatchMapping()
-    public ResponseEntity<ApiResponse<Void>> changeUserInfo(@AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                            @Valid @RequestBody MypageChangeRequestDto mypageChangeRequestDto) {
+    public ResponseEntity<ApiResponse<Void>> changeUserInfo(
+        @AuthenticationPrincipal PrincipalDetails principalDetails,
+        @RequestPart(required = false) String nickName,
+        @RequestPart(required = false) List<MultipartFile> images) {
 
-        mypageService.changeUserInfo(principalDetails.getUser(), mypageChangeRequestDto);
+        mypageService.changeUserInfo(principalDetails.getUser(), nickName, images);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseConstants.RESPONSE_UPDATE_OK);
     }
 
     @GetMapping()
     public ResponseEntity<ApiResponse<ViewMypageResponseDto>> viewMypage(
-            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        @AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-        ApiResponse<ViewMypageResponseDto> response = new ApiResponse<>(200, ResponseMessages.VIEW_MESSAGE.getContent(),
-                mypageService.viewMypage(principalDetails.getUser()));
+        ApiResponse<ViewMypageResponseDto> response = new ApiResponse<>(200,
+            ResponseMessages.VIEW_MESSAGE.getContent(),
+            mypageService.viewMypage(principalDetails.getUser()));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
