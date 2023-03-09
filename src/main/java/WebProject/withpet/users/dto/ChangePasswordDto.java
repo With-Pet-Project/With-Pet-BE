22b464ebfcfced9cc865140a060db8e442f08492
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-09 14:13:20 +0900

    feat: 요구사항 변경에 따른 dto에 변수 추가

--- src/main/java/WebProject/withpet/users/dto/ChangePasswordDto.java
+++ src/main/java/WebProject/withpet/users/dto/ChangePasswordDto.java
@@ -1,5 +1,7 @@
 package WebProject.withpet.users.dto;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChangePasswordDto {
+
+    @Email(message = "이메일 형식이 아닙니다")
+    private String email;
 
     @NotBlank(message = "변경할 비밀번호를 입력하세요")
     private String password;
