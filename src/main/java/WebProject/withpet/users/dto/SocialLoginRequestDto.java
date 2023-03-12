e996a8925b8d42044a4bc7098f1e71a12671fc81
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-12 21:08:25 +0900

    feat: redirect URI 정보를 받는 dto 추가

---
+++ src/main/java/WebProject/withpet/users/dto/SocialLoginRequestDto.java
@@ -0,0 +1,14 @@
+package WebProject.withpet.users.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialLoginRequestDto {
+
+    private String redirectURI;
+}
