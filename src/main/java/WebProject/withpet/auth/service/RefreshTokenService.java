076e4e2b10e557359423495ed426bfbf6322a171
Jin409 <sualng123@gmail.com>
2023-03-12 21:11:48 +0900

    feat: RefreshToken 유효성 검증 기능 구현

--- src/main/java/WebProject/withpet/auth/service/RefreshTokenService.java
+++ src/main/java/WebProject/withpet/auth/service/RefreshTokenService.java
@@ -2,6 +2,7 @@ package WebProject.withpet.auth.service;
 
 import WebProject.withpet.auth.domain.RefreshToken;
 import WebProject.withpet.auth.domain.RefreshTokenRepository;
+import WebProject.withpet.common.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +14,17 @@ public class RefreshTokenService {
 
     @Transactional
     public void createOrChangeRefreshToken(String refreshToken, Long userId) {
-        refreshTokenRepository.findByUserId(userId).ifPresentOrElse(
-                (tokenEntity) -> tokenEntity.changeToken(refreshToken),
-                () -> refreshTokenRepository.save(new RefreshToken(refreshToken, userId))
-        );
+        refreshTokenRepository.findByUserId(userId)
+                .ifPresentOrElse((tokenEntity) -> tokenEntity.changeToken(refreshToken),
+                        () -> refreshTokenRepository.save(new RefreshToken(refreshToken, userId)));
+    }
+
+    public boolean isValidToken(String token, Long userId) {
+        RefreshToken refreshToken = findTokenByUserId(userId);
+        return refreshToken.getContent().equals(token);
+    }
+
+    private RefreshToken findTokenByUserId(Long userId) {
+        return refreshTokenRepository.findByUserId(userId).orElseThrow(DataNotFoundException::new);
     }
 }
