c2ddbe8a31ed2f2268e8fc37ca0fa2f46248706a
Jin409 <sualng123@gmail.com>
2023-03-17 00:30:44 +0900

    feat: 액세스 토큰 재발급과 리프레시 토큰 유효기간에 따른 재발급 기능 구현

--- src/main/java/WebProject/withpet/auth/controller/AccessTokenController.java
+++ src/main/java/WebProject/withpet/auth/controller/AccessTokenController.java
@@ -1,11 +1,17 @@
 package WebProject.withpet.auth.controller;
 
 import static WebProject.withpet.auth.application.JwtTokenProvider.REFRESH_TOKEN_HEADER_STRING;
+import static WebProject.withpet.auth.util.JwtUtil.COOKIE_NAME;
 
-import WebProject.withpet.auth.application.JwtTokenProvider;
+import WebProject.withpet.auth.dto.TokenResponseDto;
+import WebProject.withpet.auth.service.TokenService;
+import WebProject.withpet.auth.util.JwtUtil;
 import WebProject.withpet.common.dto.ApiResponse;
+import java.util.Date;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/access")
+@RequestMapping("/reissue")
 public class AccessTokenController {
 
-    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtUtil jwtUtil;
+    private final TokenService tokenService;
 
     @GetMapping
     public ResponseEntity<ApiResponse<String>> getNewAccessToken(
+            @CookieValue(name = REFRESH_TOKEN_HEADER_STRING) String refreshToken, HttpServletResponse response) {
+        Long userId = jwtUtil.getUserId(refreshToken);
+        TokenResponseDto responseDto = tokenService.reissueToken(refreshToken, userId, new Date());
+        String newRefreshToken = responseDto.getRefreshToken();
+        if (responseDto.getRefreshToken() != null) {
+            ResponseCookie cookie = jwtUtil.createCookie(newRefreshToken);
+            response.addHeader(COOKIE_NAME, cookie.toString());
+        }
 
-            @CookieValue(name = REFRESH_TOKEN_HEADER_STRING) String refreshToken) throws Exception {
-        String newAccessToken = jwtTokenProvider.getNewAccessToken(refreshToken);
-        return ResponseEntity.ok(new ApiResponse<>(200, "새로운 액세스 토큰이 발급되었습니다.", newAccessToken));
+        return ResponseEntity.ok(new ApiResponse<>(200, "새로운 액세스 토큰이 발급되었습니다.", responseDto.getAccessToken()));
     }
 }
