de38a86d914c6e939270f555fbe529df066e0251
Jin409 <sualng123@gmail.com>
2023-03-22 22:11:47 +0900

    refactor: AuthenticationFilter 가 사용되지 않음에 따라 삭제

--- src/main/java/WebProject/withpet/common/config/SecurityConfig.java
+++ src/main/java/WebProject/withpet/common/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package WebProject.withpet.common.config;
 
 import WebProject.withpet.auth.application.JwtTokenProvider;
-import WebProject.withpet.auth.filter.JwtAuthenticationFilter;
 import WebProject.withpet.auth.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -29,16 +28,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManager authentication = authenticationManager(authenticationConfiguration);
-        JwtAuthenticationFilter authenticationFilter = new JwtAuthenticationFilter(authentication,
-                jwtTokenProvider);
 
         http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 
                 .formLogin().disable().httpBasic().disable()
-
-                .addFilter(authenticationFilter)
                 .addFilter(new JwtAuthorizationFilter(authentication, jwtTokenProvider))
                 .authorizeRequests();
 
--- src/main/java/WebProject/withpet/common/dto/ApiErrorResponse.java
+++ src/main/java/WebProject/withpet/common/dto/ApiErrorResponse.java
@@ -8,13 +8,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.validation.ConstraintViolation;
-import javax.validation.Path;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -37,55 +34,55 @@ public class ApiErrorResponse {
 
     public static ResponseEntity<ApiErrorResponse> toResponseEntity(ErrorCode e) {
         return ResponseEntity
-            .status(e.getHttpStatus())
-            .body(
-                ApiErrorResponse.builder()
-                    .code(e.getHttpStatus().value())
-                    .message(e.getMessage())
-                    .build());
+                .status(e.getHttpStatus())
+                .body(
+                        ApiErrorResponse.builder()
+                                .code(e.getHttpStatus().value())
+                                .message(e.getMessage())
+                                .build());
     }
 
     public static ResponseEntity<ApiErrorResponse> toResponseEntityWithErrors(ErrorCode e,
-        Errors errors) {
+                                                                              Errors errors) {
         ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
-            .code(e.getHttpStatus().value())
-            .message(e.getMessage())
-            .build();
+                .code(e.getHttpStatus().value())
+                .message(e.getMessage())
+                .build();
         apiErrorResponse.setCustomFieldErrors(errors.getFieldErrors());
 
         return ResponseEntity
-            .status(e.getHttpStatus())
-            .body(apiErrorResponse);
+                .status(e.getHttpStatus())
+                .body(apiErrorResponse);
     }
 
     public static ResponseEntity<ApiErrorResponse> toResponseEntityWithRequestParameterException(
-        ErrorCode e,
-        MissingServletRequestParameterException ex) {
+            ErrorCode e,
+            MissingServletRequestParameterException ex) {
         ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
-            .code(e.getHttpStatus().value())
-            .message(e.getMessage())
-            .build();
+                .code(e.getHttpStatus().value())
+                .message(e.getMessage())
+                .build();
 
         apiErrorResponse.setCustomFieldErrors(ex);
 
         return ResponseEntity
-            .status(e.getHttpStatus())
-            .body(apiErrorResponse);
+                .status(e.getHttpStatus())
+                .body(apiErrorResponse);
 
     }
 
     public static ResponseEntity<ApiErrorResponse> toResponseEntityWithConstraints(ErrorCode e,
-        Set<ConstraintViolation<?>> violations
+                                                                                   Set<ConstraintViolation<?>> violations
     ) {
         ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
-            .code(e.getHttpStatus().value())
-            .message(e.getMessage())
-            .build();
+                .code(e.getHttpStatus().value())
+                .message(e.getMessage())
+                .build();
         apiErrorResponse.setConstraintMessages(violations);
 
         return ResponseEntity
-            .status(e.getHttpStatus())
-            .body(apiErrorResponse);
+                .status(e.getHttpStatus())
+                .body(apiErrorResponse);
     }
 
     public void setCustomFieldErrors(List<FieldError> fieldErrors) {
@@ -94,9 +91,9 @@ public class ApiErrorResponse {
 
         fieldErrors.forEach(error -> {
             customFieldErrors.add(new CustomFieldError(
-                error.getField(),
-                error.getRejectedValue(),
-                error.getDefaultMessage()
+                    error.getField(),
+                    error.getRejectedValue(),
+                    error.getDefaultMessage()
             ));
         });
     }
@@ -106,19 +103,19 @@ public class ApiErrorResponse {
         customFieldErrors = new ArrayList<>();
 
         customFieldErrors.add(CustomFieldError.builder()
-            .field(ex.getParameterName())
-            .reason(ex.getMessage())
-            .build()
+                .field(ex.getParameterName())
+                .reason(ex.getMessage())
+                .build()
         );
     }
 
     public void setConstraintMessages(Set<ConstraintViolation<?>> violations) {
 
         this.constraintMessages = violations
-            .stream()
-            .map(s -> new CustomFieldError(s.getPropertyPath().toString(), s.getInvalidValue(),
-                s.getMessage()))
-            .collect(Collectors.toList());
+                .stream()
+                .map(s -> new CustomFieldError(s.getPropertyPath().toString(), s.getInvalidValue(),
+                        s.getMessage()))
+                .collect(Collectors.toList());
 
 
     }
