2d1d8a27b44180eafb27a0ef0b41128598783e84
Jin409 <sualng123@gmail.com>
2023-03-24 00:12:18 +0900

    feat: 챌린지 로그 저장 이전 중복 확인 기능 구현

--- src/main/java/WebProject/withpet/challenges/service/ChallengeLogService.java
+++ src/main/java/WebProject/withpet/challenges/service/ChallengeLogService.java
@@ -3,10 +3,10 @@ package WebProject.withpet.challenges.service;
 import WebProject.withpet.challenges.domain.Challenge;
 import WebProject.withpet.challenges.domain.ChallengeLog;
 import WebProject.withpet.challenges.domain.ChallengeLogRepository;
-import WebProject.withpet.challenges.domain.ChallengeLogRepositoryImpl;
-import WebProject.withpet.challenges.domain.ChallengeRepository;
 import WebProject.withpet.challenges.dto.ChallengeLogRequestDto;
+import WebProject.withpet.common.constants.ErrorCode;
 import WebProject.withpet.common.exception.DataNotFoundException;
+import WebProject.withpet.common.exception.DuplicateDateException;
 import WebProject.withpet.pets.service.PetService;
 import WebProject.withpet.users.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +16,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChallengeLogService {
     private final ChallengeLogRepository challengeLogRepository;
-    private final ChallengeLogRepositoryImpl challengeLogRepositoryImpl;
-    private final ChallengeRepository challengeRepository;
     private final ChallengeService challengeService;
     private final PetService petService;
 
     public void registerChallengeLog(Long petId, Long challengeId, User user, ChallengeLogRequestDto requestDto) {
         Challenge challenge = challengeService.accessChallenge(petId, challengeId, user);
+        if (isDuplicateChallengeLog(challenge, requestDto)) {
+            throw new DuplicateDateException(ErrorCode.DUPLICATE_DATE);
+        }
         challengeLogRepository.save(requestDto.toEntity(challenge));
     }
 
@@ -38,5 +39,10 @@ public class ChallengeLogService {
     private ChallengeLog accessChallengeLog(Long id, Long petId, User user) {
         petService.accessPet(user, petId);
         return findChallengeLogById(id);
+    }
+
+    private boolean isDuplicateChallengeLog(Challenge challenge, ChallengeLogRequestDto requestDto) {
+        return challengeLogRepository.isDuplicateChallengeLog(challenge, requestDto.getYear(), requestDto.getMonth(),
+                requestDto.getWeek(), requestDto.getDay());
     }
 }
